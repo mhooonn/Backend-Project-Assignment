5a367f2a03b49c4f7e1bfb21c836f3949b1deafd
mhooonn <max.hallenberg@hotmail.com>
2026-04-25 14:48:35 +0300

    save conversion

---
+++ controllers/history.js
@@ -0,0 +1,15 @@
+const Conversion = require ('/models/conversion');
+
+// save conversion to db
+const saveConversion = async (req, res) => {
+  try {
+    const {from, to, amount, result} = req.body;
+
+    const conversion = await Conversion.create({from, to, amount, result});
+
+    res.status(201).json(conversion);
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
