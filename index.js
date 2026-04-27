b61e4a86aeedb6e2e39f7c54a91a88d730ffb390
Kalle Saarinen <kallemikael96@gmail.com>
2026-04-27 21:58:23 +0300

    Added imports and added the POST route to index.js

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ require("dotenv").config();
 
 const app = express();
 
+const { body } = require("express-validator");
+const { convertCurrency } = require("./controllers/convertController");
+
 // 🔹 import routes
 const convertRoutes = require("./routes/convertRoutes");
 const Conversion = require("./models/Conversion");
@@ -43,6 +46,31 @@ app.post("/history/:id/delete", async (req, res) => {
 });
 
 app.use("/api/conversions", convertRoutes);
+
+app.post(
+  "/convert",
+  [
+    body("amount")
+      .isFloat({ gt: 0 })
+      .withMessage("Amount must be a positive number"),
+
+    body("from")
+      .notEmpty()
+      .withMessage("From currency is required"),
+
+    body("to")
+      .notEmpty()
+      .withMessage("To currency is required"),
+
+    body("to").custom((value, { req }) => {
+      if (value === req.body.from) {
+        throw new Error("Currencies must be different");
+      }
+      return true;
+    })
+  ],
+  convertCurrency
+);
 
 // 🔹 server
 const PORT = process.env.PORT || 3000;
